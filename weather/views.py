cb00e71de37e9a9ee9c3a5512cb8900c5732deab
Anastasiya Chultsova <anastasiya.chultsova@itechart-group.com>
2022-02-25 19:42:43 +0300

    new commit

--- weather/views.py
+++ weather/views.py
@@ -1,19 +1,29 @@
 import requests 
 from django.shortcuts import render
+from .models import City
 
 def index(request):
     appId= '08ce24b022ca7bd3edcdafc3a16d69ca'
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appId
 
-    city = 'London'
-    res = requests.get(url.format(city)).json()
     
-    cityInfo = {
-        'city': city,
-        'temp': res["main"] ["temp"],
-        'icon': res["weather"] [0] ["icon"]
-    }
+    
 
-    context = {'info': cityInfo}
+    cities = City.objects.all()
+
+
+    allCities = []
+
+    for city in cities:
+        res = requests.get(url.format(city)).json()
+        cityInfo = {
+            'city': city.name,
+            'temp': res["main"] ["temp"],
+            'icon': res["weather"] [0] ["icon"]
+        }
+
+        allCities.append(cityInfo)
+
+    context = {'allInfo': cityInfo}
 
     return render(request, 'weather/index.html', context)
