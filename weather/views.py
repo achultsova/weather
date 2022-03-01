b3ed97c4677461aed713ab381d025daadc49b85c
Anastasiya Chultsova <anastasiya.chultsova@itechart-group.com>
2022-03-01 15:42:52 +0300

    simple forecast

--- weather/views.py
+++ weather/views.py
@@ -1,12 +1,17 @@
 import requests 
 from django.shortcuts import render
 from .models import City
+from .forms import CityForm
 
 def index(request):
     appId= '08ce24b022ca7bd3edcdafc3a16d69ca'
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appId
 
-    
+    if(request.method == 'POST'):
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()    
     
 
     cities = City.objects.all()
@@ -15,7 +20,7 @@ def index(request):
     allCities = []
 
     for city in cities:
-        res = requests.get(url.format(city)).json()
+        res = requests.get(url.format(city.name)).json()
         cityInfo = {
             'city': city.name,
             'temp': res["main"] ["temp"],
@@ -24,6 +29,6 @@ def index(request):
 
         allCities.append(cityInfo)
 
-    context = {'allInfo': cityInfo}
+    context = {'allInfo': allCities, 'form': form}
 
     return render(request, 'weather/index.html', context)
